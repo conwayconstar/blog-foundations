e03013ab72150a3f6e03a0ed6253b057cb87fc1f
Connor Mulhall <connor@onacob.com>
2018-10-22 18:39:46 +0100

    User Views

---
+++ frontend/pages/account.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        Hello {{ user.fullName | ucFirst }}
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    Logo
+  },
+  filters: {
+    ucFirst(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+  },
+  computed: {
+    ...mapState('account', ['user'])
+  }
+}
+</script>
