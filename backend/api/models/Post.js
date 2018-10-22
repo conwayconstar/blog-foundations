76dee953d7a0f0f3a34d1bc69fb4da0bdb9d500d
Connor Mulhall <connor@onacob.com>
2018-10-22 18:04:58 +0100

    Post Model

---
+++ backend/api/models/Post.js
@@ -0,0 +1,59 @@
+/**
+ * Post.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    title: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    content: {
+      type: 'text'
+    },
+
+    slug: {
+      type: 'slug',
+      from: 'title',
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    user: {
+      model: 'user',
+      required: true
+    },
+
+    comments: {
+      collection: 'comment',
+      via: 'post'
+    },
+
+    categories: {
+      collection: 'category',
+      via: 'posts'
+    }
+
+  },
+
+};
+
